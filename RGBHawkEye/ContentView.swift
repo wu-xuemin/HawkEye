f79f671e29e7bdc3fa93cac1f8b629c4011d660d
wu-xuemin <wuxuemin2000@126.com>
2020-03-16 18:04:40 +0900

    Initial Commit

---
+++ RGBHawkEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBHawkEye
+//
+//  Created by wxm on 2020/3/16.
+//  Copyright © 2020 wxm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
